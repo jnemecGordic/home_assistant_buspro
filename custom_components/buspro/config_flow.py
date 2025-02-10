e165135a1a8161ccf5fc3930e78609bc3b8f6ba0
Jiří Němec <jiri_nemec@gordic.cz>
2025-02-10 14:09:35 +0100

    Configuration flow fixed

--- custom_components/buspro/config_flow.py
+++ custom_components/buspro/config_flow.py
@@ -3,42 +3,61 @@ import logging
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.core import callback
 
-from .const import (
-    DOMAIN,
-)
+from .const import DOMAIN
 
 from homeassistant.const import (
     CONF_BROADCAST_ADDRESS,
     CONF_BROADCAST_PORT,
 )
+
 _LOGGER = logging.getLogger(__name__)
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
-    
+
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         errors = {}
-        
+
         if user_input is not None:
             try:
                 return self.async_create_entry(title="Buspro", data=user_input)
-                    
             except Exception:
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
 
         return self.async_show_form(
             step_id="user",
-
             data_schema=vol.Schema({
-            	vol.Required(CONF_BROADCAST_ADDRESS, default="192.168.10.255"): cv.string,
-            	vol.Required(CONF_BROADCAST_PORT, default=6000): cv.port
-            	#vol.Required(CONF_BROADCAST_ADDRESS): cv.string,
-            	#vol.Required(CONF_BROADCAST_PORT): cv.port
+                vol.Required(CONF_BROADCAST_ADDRESS, default="192.168.10.255"): cv.string,
+                vol.Required(CONF_BROADCAST_PORT, default=6000): cv.port
             }),
             errors=errors
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return BusproOptionsFlowHandler(config_entry)
+
+class BusproOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        if user_input is not None:
+            self.hass.config_entries.async_update_entry(
+                self.config_entry, data=user_input
+            )
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Required(CONF_BROADCAST_ADDRESS, default=self.config_entry.data.get(CONF_BROADCAST_ADDRESS, "192.168.10.255")): cv.string,
+                vol.Required(CONF_BROADCAST_PORT, default=self.config_entry.data.get(CONF_BROADCAST_PORT, 6000)): cv.port
+            })
         )
